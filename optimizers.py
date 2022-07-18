f81558677da95cc2902ab502e41e183ebbabdfe9
mirco-gnuva <mirco.gnuva@gmail.com>
2022-07-18 17:18:03 +0200

    Implemented BHOptimizer: O_R optimizer based on Basin Hopping

--- optimizers.py
+++ optimizers.py
@@ -3,6 +3,7 @@ from generator import RandomGenerator
 from tqdm import tqdm
 from skopt.space import Real
 from skopt import gp_minimize
+from scipy.optimize import basinhopping
 
 
 class Optimizer:
@@ -72,3 +73,45 @@ class GPOptimizer(Optimizer):
 
     def get_space(self):
         return [Real(low=0, high=1)] * self.tuples_size
+
+
+class BHOptimizer(Optimizer):
+    def __init__(self, mat_D, vec_T, mat_P, n_tuples=1000 * 1000, tuples_size=3, guess: tuple = (0.5, 0.5, 0.5),
+                 timeout=1000, min_imp=0.01, min_imp_timeout=100):
+        super().__init__(mat_D, vec_T, mat_P)
+        self.n_tuples = n_tuples
+        self.tuples_size = tuples_size
+        self.guess = guess
+        self.timeout = timeout
+        self.min_imp = min_imp
+        self.min_imp_timeout = min_imp_timeout
+
+    def optimize(self):
+        result = basinhopping(self.get_O_R, self.guess, niter=self.n_tuples, niter_success=self.timeout)
+
+        self.best_O_R = result.fun
+        self.best_tuple = result.x
+
+        return {'best_tuple': self.best_tuple, 'best_O_R': self.best_O_R}
+
+    def save_minimum(self, x, O_R, accepted):
+        if not accepted or not O_R < self.best_O_R:
+            return None
+
+        positive = self.positive_alphas(x)
+        if positive:
+            old_best_O_R = self.best_O_R
+            self.best_O_R = O_R
+            self.best_tuple = x
+
+            if old_best_O_R - O_R < self.min_imp:
+                self.min_imp_timeout -= 1
+
+                return self.min_imp_timeout < 0
+
+    @staticmethod
+    def positive_alphas(alphas):
+        for alpha in alphas:
+            if alpha < 0:
+                return False
+        return True

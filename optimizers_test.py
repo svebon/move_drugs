04544f7a1ec7f4550e0ee1a2f2f8041d295b37e3
mirco-gnuva <mirco.gnuva@gmail.com>
2022-07-25 15:47:00 +0200

    Integrated classes' updates

--- optimizers_test.py
+++ optimizers_test.py
@@ -1,35 +1,35 @@
-from optimizers import *
-import pandas as pd
+import yaml
 from data_parsing import *
+from optimizers import *
+
 
 # Data's paths
-input_D = 'C:/Users/mirco/Projects/Sveva/ML_datasets/matrix_D.csv'
-input_T = 'C:/Users/mirco/Projects/Sveva/ML_datasets/vector_T.csv'
-input_P = 'C:/Users/mirco/Projects/Sveva/ML_datasets/matrix_P.csv'
-nodes = 'C:/Users/mirco/Projects/Sveva/ML_datasets/new_mapped_name.csv'
+with open('paths.yaml') as f:
+    paths = yaml.load(f, Loader=yaml.SafeLoader)
 
 # Data loading
-D = DockingEnergies()
-
 names_map = NamesMap()
-names_map.load(nodes)
+names_map.load(paths['nodes'])
 
-T = Topology(names_map)
+D = DockingEnergies(names_map)
+T = Topology()
+P = Interactions(names_map)
 
-P = Interactions()
+D.load(paths['input_D'])
+T.load(paths['input_T'])
+P.load(paths['input_P'])
 
-D.load(input_D)
-T.load(input_T)
-P.load(input_P)
+common_recs = common_receptors(D.receptors, T.receptors, P.receptors)
 
-D.filter_receptors(T.receptors)
-P.filter_receptors(T.receptors)
+D.filter_receptors(common_recs)
+T.filter_receptors(common_recs)
+P.filter_receptors(common_recs)
 
-#optimizer = RandomOptimizer(D.to_numpy(), T.to_numpy(), P.to_numpy())
-#optimizer = GPOptimizer(D.to_numpy(), T.to_numpy(), P.to_numpy(), min_imp_timeout=50)
+# optimizer = RandomOptimizer(D.to_numpy(), T.to_numpy(), P.to_numpy())
+# optimizer = GPOptimizer(D.to_numpy(), T.to_numpy(), P.to_numpy(), min_imp_timeout=50)
 optimizer = BHOptimizer(D.to_numpy(), T.to_numpy(), P.to_numpy(), min_imp_timeout=10, guess=[1, 1, 1])
 result = optimizer.optimize()
 
-print(f'Best tuple: {result["best_tuple"]}')
-print(f'Best O_R: {result["best_O_R_avg"]}')
+print(f'Best tuple: {result.alphas}')
+print(f'Best O_R: {result.O_R_avg}')
 
